216d8490f10749b520731bcae235b1aec62e9d6e
Paulo Victor <pv.35@hotmail.com>
2025-04-08 16:05:07 +0100

    created struct and enums

--- philo/header/philosophers.h
+++ philo/header/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 15:42:34 by paude-so          #+#    #+#             */
-/*   Updated: 2025/04/08 15:50:18 by paude-so         ###   ########.fr       */
+/*   Updated: 2025/04/08 16:04:51 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,40 @@
 # include <sys/types.h>
 # include "ft_utils.h"
 
+typedef enum e_philo_state
+{
+	THINKING,
+	EATING,
+	SLEEPING,
+	WAITING,
+	FORK_TAKEN,
+	FORK_AVAILABLE,
+	FORK_USED,
+	FORK_RETURNED
+}	t_philo_state;
+
+typedef enum e_philo_action
+{
+	TAKE_FORK,
+	EAT,
+	SLEEP,
+	THINK,
+}	t_philo_action;
+
+typedef enum e_philo_status
+{
+	ALIVE,
+	DEAD,
+	FULL
+}	t_philo_status;	
+
 typedef struct s_philo
 {
 	int	num_philo;
 	int	time_to_die;
 	int	time_to_eat;
 	int	time_to_sleep;
-	
+	int	num_eat;
 }	t_philo;
 
 #endif
