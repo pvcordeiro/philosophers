89adbafcd9aaffd1766716fbca557e1547629fa9
Paulo Victor <pv.35@hotmail.com>
2025-04-17 17:45:59 +0100

    created some philos

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 20:43:38 by paude-so          #+#    #+#             */
-/*   Updated: 2025/04/15 17:06:50 by paude-so         ###   ########.fr       */
+/*   Updated: 2025/04/17 17:44:01 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,16 +41,20 @@ t_all *all(void)
 void	create_philos(void)
 {
 	size_t	i;
-	t_philo philo[all()->num_philo];
+	t_philo *philo;
 	// pthread_t threads[all()->num_philo];
 
 	i = 0;
-	while (i < all()->num_philo)
+	while (++i <= all()->num_philo)
 	{
+		philo = malloc(sizeof(t_philo));
+		if (!philo)
+			return ;
 		philo->id = i;
-		ft_list_add(&all()->philos, &philo, free);
+		philo->status = ALIVE;
+		philo->meals = 0;
+		ft_list_add(&all()->philos, philo, free);
 		// pthread_create(&threads[i], NULL, philo_routine, &philo[i]);
-		i++;
 	}
 }
 
@@ -64,7 +68,7 @@ bool	positive(long long arg)
 void	init_all(int argc, char **argv)
 {
 	int i = 0;
-	while (++i <= argc)
+	while (++i < argc)
 	{
 		if (!positive(ft_atoll(argv[i])))
 			exit(ft_printf("Only positive numbers allowed\n"));
@@ -73,21 +77,28 @@ void	init_all(int argc, char **argv)
 	all()->time_to_die = ft_atoll(argv[2]);
 	all()->time_to_eat = ft_atoll(argv[3]);
 	all()->time_to_sleep = ft_atoll(argv[4]);
-	if (argc == 5)
+	if (argc == 6)
 		all()->num_eat = ft_atoll(argv[5]);
 }
 
 int	main(int argc, char **argv)
 {
-	if (argc < 5)
+	t_list	*current;
+	t_philo	*philosopher;
+
+	if (argc < 5 || argc > 6)
 		return (ft_printf("Usage: number_of_philosophers time_to_die time_to_eat time_to_sleep Optional: [number_of_times_each_philosopher_must_eat]\n"));
 	init_all(argc, argv);
 	create_philos();
 	ft_printf("number_of_philosophers: %d\ntime_to_die:%d\ntime_to_eat:%d\ntime_to_sleep:%d\n", all()->num_philo, all()->time_to_die, all()->time_to_eat, all()->time_to_sleep);
-	if (all()->num_eat)
+	if (argc == 6)
 		ft_printf("number_of_times_each_philosopher_must_eat:%d\n", all()->num_eat);
-	int i = -1;
-	while(all()->philos)
-		ft_printf("philo number: %d", all()->philos[i]);
+	current = all()->philos;
+	while(current)
+	{
+		philosopher = (t_philo *)current->data;
+		ft_printf("\nphilo number: %d\nphilo address: %p", philosopher->id, philosopher);
+		current = current->next;
+	}
 	return (0);
 }
