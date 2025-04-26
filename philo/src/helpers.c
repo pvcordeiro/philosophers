af5001604d94d369d27b410bd90e7f22676ecc1f
Paulo Victor <pv.35@hotmail.com>
2025-04-26 12:48:30 +0100

    norminette left

--- philo/src/helpers.c
+++ philo/src/helpers.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 13:13:33 by paude-so          #+#    #+#             */
-/*   Updated: 2025/04/26 12:20:04 by paude-so         ###   ########.fr       */
+/*   Updated: 2025/04/26 12:44:30 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,10 +57,8 @@ bool	philo_alive(t_philo *philo)
 {
 	bool	alive;
 
-	pthread_mutex_lock(&all()->data_mutex);
 	pthread_mutex_lock(&philo->philo_mutex);
 	alive = philo->status == ALIVE && !all()->dead_philo;
 	pthread_mutex_unlock(&philo->philo_mutex);
-	pthread_mutex_unlock(&all()->data_mutex);
 	return (alive);
 }
